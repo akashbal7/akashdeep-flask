7caf02ae310f2d9212a570a12d49a080e2915bdb
Akashdeep Singh <singhakash0796@gmail.com>
2024-12-05 10:03:45 -0500

    sdjkjs

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 AUTHY_API_KEY = "dpU6FwOcrwJsEOMZ0KE2LpvsDWuiHqT1"
 SECRET_KEY = "restaurant"
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@database-1.c12ceie2m5t0.us-east-1.rds.amazonaws.com:5432/restaurant_db'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@restaurant-db.cluster-c12ceie2m5t0.us-east-1.rds.amazonaws.com:5432/restaurant_db'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@host.docker.internal:5432/restaurant_db'
 
